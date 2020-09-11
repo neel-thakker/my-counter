0febc691fa7e0adec583731d83155efff9c3975a
Neel Thakker <neelthakker2001@gmail.com>
2020-09-11 17:39:17 +0530

    Uploading to github

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="navbar-brand">
-        Navbar
+        Navbar &nbsp;
         <span className="badge badge-pill badge-secondary">
           {totalCounters}
         </span>
